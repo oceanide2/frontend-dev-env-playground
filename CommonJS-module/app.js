20a0a30775801c00a4574967c1707259ddf62f2f
oceanide <oceanide@gmail.com>
2021-05-17 15:56:53 +0900

    Add test code for CommonJS module

---
+++ CommonJS-module/app.js
@@ -0,0 +1,9 @@
+const { sum, subtract, multiply, divide } = require('./calculator.js');
+
+const a = 20;
+const b = 10;
+
+console.log(sum(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
---
+++ CommonJS-module/math.js
@@ -0,0 +1,10 @@
+const calculator = require('./calculator.js');
+
+const a = 20;
+const b = 10;
+
+console.log(calculator.sum(a, b));
+console.log(calculator.subtract(a, b));
+console.log(calculator.multiply(a, b));
+console.log(calculator.divide(a, b));
+
