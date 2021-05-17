6f63321639b33a040a1315eec7c4119407d1c6a7
oceanide <oceanide@gmail.com>
2021-05-17 15:57:31 +0900

    Add test code for ES2015 module

---
+++ ES2015-module/app.js
@@ -0,0 +1,10 @@
+import { sum, subtract, multiply, divide } from './calculator.js';
+
+const a = 20;
+const b = 10;
+
+console.log('app.js');
+console.log(sum(a, b));
+console.log(subtract(a, b));
+console.log(multiply(a, b));
+console.log(divide(a, b));
---
+++ ES2015-module/math.js
@@ -0,0 +1,10 @@
+import * as calculator from './calculator.js';
+
+const a = 20;
+const b = 10;
+
+console.log('math.js');
+console.log(calculator.sum(a, b));
+console.log(calculator.subtract(a, b));
+console.log(calculator.multiply(a, b));
+console.log(calculator.divide(a, b));
